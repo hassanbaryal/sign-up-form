aa42c2052e71c887421a191d157b8a76db29eac3
hassanbaryal <h.baryal2@gmail.com>
2022-06-07 18:56:57 -0400

    Cleaned up the code a bit. Added a few comments

--- script.js
+++ script.js
@@ -7,17 +7,21 @@ const confirmPassword = document.querySelector('#confirm-password')
 const submitButton = document.querySelector('.submit-btn')
 const signUpForm = document.querySelector('#sign-up-form')
 
+
 signUpForm.addEventListener("submit", (e) =>{validateForm(e)})
 confirmPassword.addEventListener('keyup', (e)=>{checkPasswords(e)})
 
+// for toggling light to dark them and vice versa, whenever this feature is added
 root.classList.add('light')
 
+// Prevents form from being submitted if passwords do not match
 function validateForm (e) {
     if (password.value != confirmPassword.value) {
         e.preventDefault()
     }
 }
 
+// checks if both passwords match when a key is pressed on confirm password field
 function checkPasswords (e) {
     if(password.value == confirmPassword.value) {
         para.classList.remove('error')
